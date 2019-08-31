d0a7fc42f07a035c42b025f79907ae539de6afd3
Noah Lubko <nlubko@gmail.com>
2019-08-31 09:16:57 +0300

    add trakc html

--- src/app/pages/tracks/track/track.component.ts
+++ src/app/pages/tracks/track/track.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Track } from '@common/modals';
 @Component({
   selector: 'track',
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.css']
 })
 export class TrackComponent implements OnInit {
+  track: Track = {
+    track_id: 4188287,
+    date_added: new Date(),
+    artist_name: 'The Jackson 5',
+    track_name: 'I Want You Back',
+    track_duration: 0,
+    album: {
+      title: 'The Definitive Collection',
+      cover_small: '',
+      cover_medium: 'https://cdns-images.dzcdn.net/images/cover/6097a6304e58084181a2419595d88945/250x250-000000-80-0-0.jpg',
+      cover_big: 'https://cdns-images.dzcdn.net/images/cover/6097a6304e58084181a2419595d88945/500x500-000000-80-0-0.jpg',
+      cover_xl: 'https://cdns-images.dzcdn.net/images/cover/6097a6304e58084181a2419595d88945/1000x1000-000000-80-0-0.jpg'
+    }
+  };
 
   constructor(
     private _route: ActivatedRoute) { }
 
   ngOnInit() {
     // this._route.params.subscribe(params => console.log(params));
+  }
+
+  getTrackCover(track: Track) {
+    return track ? `url(${track.album.cover_big})` : null;
   }
 
 }
